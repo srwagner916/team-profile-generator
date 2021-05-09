8acf79b96472eefbb0487581662741182d36f29d
Shay Wagner <srwagner916@gmail.com>
2021-05-09 13:08:13 -0500

    will prompt information for manager then ask role of next employee

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager.js');
 
-const Managerquestions = [
+let teamArr = [];
+
+const managerQuestions = [
+  // Manager Questions
   {
     type: 'input',
     name: 'managerName',
@@ -20,13 +24,61 @@ const Managerquestions = [
     type: 'input',
     name: 'managerOfficeNumber',
     message: "What is your team manager's office number?"
-  },
-  {
-    type: 'list',
-    name: 'addAnotherTeamMember',
-    message: 'Would you like to add an engineer, intern, or finish?',
-    choices: ['Engineer', 'Intern', 'Finish']
   }
 ];
 
-const Engineerquestions
+const anotherMember = () => inquirer.prompt([
+    {
+      type: 'list',
+      name: 'addAnotherTeamMember',
+      message: 'Would you like to add an engineer, intern, or finish?',
+      choices: ['Engineer', 'Intern', 'Finish']
+    }
+  ]);
+
+  
+
+  
+  // Engineer Questions
+  //   {
+    //     type: 'input',
+    //     name: 'engineerName',
+    //     message: "What is your engineer's name?",
+    //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
+    //   },
+    //   {
+      //     type: 'input',
+      //     name: 'engineerId',
+      //     message: "What is your engineer's employee ID?",
+      //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
+      //   },
+      //   {
+        //     type: 'input',
+        //     name: 'engineerEmail',
+        //     message: "What is your engineer's email?",
+        //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
+        //   },
+        //   {
+          //     type: 'input',
+          //     name: 'engineeerGithub',
+          //     message: "What is your engineer's GitHub username",
+          //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
+          //   },
+          //   {
+            //     type: 
+            //   }
+            // ];
+            
+const managerPrompt = () => {
+  return inquirer.prompt(managerQuestions)
+    .then(answers => {
+      manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      console.log(manager);
+      teamArr.push(manager);
+    });
+}
+
+
+  managerPrompt()
+    .then(anotherMember);
+  
