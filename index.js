7d0fd8c986086fed8d77c165688e706afcaa6632
Shay Wagner <srwagner916@gmail.com>
2021-05-09 14:01:49 -0500

    after manager prompts. user is prompted for intern or engineer and redirected to menu. or they can choose to finsih

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 
 let teamArr = [];
 
@@ -27,58 +29,96 @@ const managerQuestions = [
   }
 ];
 
-const anotherMember = () => inquirer.prompt([
-    {
-      type: 'list',
-      name: 'addAnotherTeamMember',
-      message: 'Would you like to add an engineer, intern, or finish?',
-      choices: ['Engineer', 'Intern', 'Finish']
-    }
-  ]);
-
-  
-
-  
-  // Engineer Questions
-  //   {
-    //     type: 'input',
-    //     name: 'engineerName',
-    //     message: "What is your engineer's name?",
-    //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
-    //   },
-    //   {
-      //     type: 'input',
-      //     name: 'engineerId',
-      //     message: "What is your engineer's employee ID?",
-      //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
-      //   },
-      //   {
-        //     type: 'input',
-        //     name: 'engineerEmail',
-        //     message: "What is your engineer's email?",
-        //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
-        //   },
-        //   {
-          //     type: 'input',
-          //     name: 'engineeerGithub',
-          //     message: "What is your engineer's GitHub username",
-          //     when: (answers) => answers.addAnotherTeamMember === 'Engineer'
-          //   },
-          //   {
-            //     type: 
-            //   }
-            // ];
-            
 const managerPrompt = () => {
   return inquirer.prompt(managerQuestions)
     .then(answers => {
       manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
       console.log(manager);
       teamArr.push(manager);
-    });
+  });
 }
 
+const anotherMember = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'addAnotherTeamMember',
+      message: 'Would you like to add an engineer, intern, or finish?',
+      choices: ['Engineer', 'Intern', 'Finish']
+    }
+  ])
+  .then(answers => {
+    switch (answers.addAnotherTeamMember) {
+      case 'Engineer':
+        engineerPrompt().then(anotherMember);
+        break;
+      case 'Intern':
+        internPrompt().then(anotherMember);
+        break;
+      case 'Finish':
+        break;
+    }
+  })
+};
+
+const engineerPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'engineerName',
+      message: "What is your engineer's name?",
+    },
+    {
+      type: 'input',
+      name: 'engineerId',
+      message: "What is your engineer's employee ID?",
+    },
+    {
+      type: 'input',
+      name: 'engineerEmail',
+      message: "What is your engineer's email?",
+    },
+    {
+      type: 'input',
+      name: 'engineeerGithub',
+      message: "What is your engineer's GitHub username",
+    }
+  ])
+    .then(answers => {
+      engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineeerGithub);
+      teamArr.push(engineer);
+   });
+};
+            
+const internPrompt = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'internName',
+      message: "What is your intern's Name?"
+    },
+    {
+      type: 'input',
+      name: 'internId',
+      message: "What is your intern's ID?"
+    },
+    {
+      type: 'input',
+      name: 'internEmail',
+      message: "What is your intern's email?"
+    },
+    {
+      type: 'input',
+      name: 'internSchool',
+      message: "What is the name of your intern's school?"
+    }
+  ])
+    .then(answers => {
+      intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+      teamArr.push(intern);
+    });
+};
 
   managerPrompt()
-    .then(anotherMember);
+    .then(anotherMember)
   
